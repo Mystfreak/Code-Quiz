02e11c7e26fe882bd261b387337e1a9698ee4ae9
Khatra Ahmed <[khatra.ahmed@gmail.com]>
2023-02-04 12:39:40 +0000

    Adding questions.

---
+++ assets/scripts/questions.js
@@ -0,0 +1,217 @@
+const questions = [
+    { 
+        // question 1
+        question: "Inside which HTML element do we put the JavaScript?",
+        option: {
+            a: "<js>",
+            b: "<script>",
+            c: "<scripting>",
+            d: "<javascript>",
+        },
+        answer: "b",
+    },
+    {
+        // question 2
+        question: "Javascript is an __________ language?",
+        option: {
+            a: "Object-Oriented",
+            b: "Object-Based",
+            c: "Procedural",
+            d: "None of the above",
+        },
+        answer: "a",
+    },
+    {
+        // question 3
+        question: "How do you call a function named myFunction?",
+        option: {
+            a: "myFunction()",
+            b: "call function myFunction()",
+            c: "call my Function()",
+        
+        },
+        answer: "a",
+    },
+    {
+        // question 4
+        question: "How to write an IF statement in JavaScript?",
+        option: {
+            a: "if i = 5",
+            b: "if i == 5 then",
+            c: "if (i == 5)",
+            d: "if i = 5 then",
+        },
+        answer: "c",
+    },
+    {
+        // question 5
+        question: "When the switch statement matches the expression with the given labels, how is the comparison done?",
+        option: {
+            a: "Both the datatype and the result of the expression are compared",
+            b: "Only the datatype of the expression is compared",
+            c: "Only the value of the expression is compared",
+            d: "None of the above",
+        },
+        answer: "a",
+    },
+    {
+        // question 6
+        question: "Upon encountering empty statements, what does the Javascript interpreter do?",
+        option: {
+            a: "Throws an error",
+            b: "Ignores the statements",
+            c: "Gives a warning",
+            d: "None of the above",
+        },
+        answer: "b",
+    },    
+    {
+        // question 7
+        question: "When the value of an operator is NULL, the typeof returned by the unary operator will be:",
+        option: {
+            a: "Boolean",
+            b: "Undefined",
+            c: "Object",
+            d: "Integer",
+        },
+        answer: "c",
+    }, 
+    {
+        // question 8
+        question: "Which of the following methods can be used to display data in some form using Javascript?",
+        option: {
+            a: "document.write()",
+            b: "console.log()",
+            c: "window.alert()",
+            d: "All of the above",
+        },
+        answer: "d",
+    },     
+    {
+        // question 9
+        question: "What keyword is used to check whether a given property is valid or not?",
+        option: {
+            a: "in",
+            b: "is in",
+            c: "exists",
+            d: "lies",
+        },
+        answer: "a",
+    },  
+    {
+        // question 10
+        question: "What is the output of the following code? <pre><code>let sum = 0; <br>const a = [1, 2, 3]; <br>function getSum(ele) { <br>   sum += ele; <br>} <br>a.forEach(getSum); <br>console.log(sum); </code></pre>",
+        option: {
+            a: "6",
+            b: "1",
+            c: "2",
+            d: "None of the above",
+        },
+        answer: "a",
+    },     
+    {
+        // question 11
+        question: "What does the following method do in JavaScript? <pre><code>toLocateString()</code></pre>",
+        option: {
+            a: "Returns a localized object representation.",
+            b: "Returns a parsed string.",
+            c: "Returns a localized string representation of an object.",
+            d: "None of the above",
+        },
+        answer: "c",
+    }, 
+    {
+        // question 12
+        question: "Which function is used to serialize an object into a JSON string in Javascript?",
+        option: {
+            a: "stringify()",
+            b: "parse()",
+            c: "convert()",
+            d: "None of the above",
+        },
+        answer: "a",
+    }, 
+    {
+        // question 13
+        question: "Which of the following are closures in Javascript?",
+        option: {
+            a: "Variables",
+            b: "Functions",
+            c: "Objects",
+            d: "All of the above",
+        },
+        answer: "d",
+    }, 
+    {
+        // question 14
+        question: "How do you round the number 7.25, to the nearest integer?",
+        option: {
+            a: "Math.rnd(7.25)",
+            b: "round(7.25)",
+            c: "rnd(7.25)",
+            d: "Math.round(7.25)",
+        },
+        answer: "d",
+    }, 
+    {
+        // question 15
+        question: "How do we write a comment in javascript?",
+        option: {
+            a: "/*  */",
+            b: "//",
+            c: "#",
+            d: "$ $",
+        },
+        answer: "b",
+    }, 
+    {
+        // question 16
+        question: "JavaScript is the same as Java.",
+        option: {
+            a: "True",
+            b: "False",
+        },
+        answer: "b",
+    }, 
+    {
+        // question 17
+        question: "Which operator is used to assign a value to a variable?",
+        option: {
+            a: "x",
+            b: "=",
+            c: "-",
+            d: "*",
+        },
+        answer: "b",
+    }, 
+    {
+        // question 18
+        question: "What will the following code return: Boolean(10 > 9)",
+        option: {
+            a: "false",
+            b: "true",
+            c: "NaN",
+        },
+        answer: "b",
+    }, 
+    {
+        // question 19
+        question: "Is JavaScript case-sensitive?",
+        option: {
+            a: "No",
+            b: "Yes",
+        },
+        answer: "b",
+    }, 
+    {
+        // question 20
+        question: "JavaScript File Has An Extension of:",
+        option: {
+            a: ".javascript",
+            b: ".xml",
+            c: ".Java",
+            d: ".Js",
+        },
+        answer: "d",
+    },              
+];
