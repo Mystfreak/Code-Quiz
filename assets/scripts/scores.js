97bf5a03d9ad81e7827145c0d793606cf3e65305
Khatra Ahmed <[khatra.ahmed@gmail.com]>
2023-03-03 20:55:56 +0000

    Trying to store user high scores and display.

--- assets/scripts/scores.js
+++ assets/scripts/scores.js
@@ -1,20 +1,62 @@
-const storage = JSON.parse(localStorage.getItem("user")) || "[]";
-const clearBtn = document.getElementById("clear");
+function saveHighscore() {
+  // Retrieve value of input box
+  var initials = initials.value.trim();
 
-function displayhighScore(storage){
-  let highScore = '';
-storage.map(
-     highScore +=
-       `<li style="list-style:none"><span class="span-score">User:</span>${user.initials}  <span class="span-score">Score:</span>${user.score}</li>`
-   )
-   highscores.innerHTML = highScore.join("");
+  if (initials !== "") {
+    // Retrieve saved scores from localstorage, or if not any, set to empty array
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
 
+    // Format new score object for current user
+    var newScore = {
+      score: time,
+      initials: initials,
+    };
 
-   clearBtn.addEventListener("click", () => {
-    localStorage.getItem("user");
-    localStorage.removeItem("user");
-    highscores.innerHTML = "";
-  });
+    // Add to localstorage
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    // Move to next page
+    window.location.href = "highscores.html";
   }
-   
+}
+
+function checkForEnter(event) {
+  //Present the enter key
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
+}
+
+
+// Getting scores from localstorage or set to an empty array
+function printHighscores() {
+  let highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+  let clearButton = document.getElementById("clear");
+
+  // Added high score in descending order
+  highscores.sort(function (a, b) {
+    return b.score - a.score;
+  });
+
+  // Adding li tag for each high score
+  highscores.forEach(function (score) {
+    let liTag = document.createElement("li");
+    liTag.textContent = score.initials + " - " + score.score;
+
+    // Appending the high score on the page
+    let olEl = document.getElementById("highscores");
+    olEl.appendChild(liTag);
+  });
+
+}
+
+
+// Running function on loaded page
+printHighscores();
+
+
+ 
+ 
 
